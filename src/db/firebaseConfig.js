7359391f2129e1d11a3b24c1f4dddbecd87e08ba
tina3008 <kicja_3@ukr.net>
2024-11-10 13:58:47 +0200

    Update firebaseConfig.js

--- src/db/firebaseConfig.js
+++ src/db/firebaseConfig.js
@@ -6,19 +6,19 @@ import { env } from '../utils/env.js';
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCkxPo19SKC6V2-8LbTZ2GtxLW5CqWoePs',
-  authDomain: 'dbproject-68bad.firebaseapp.com',
-  databaseURL: 'https://dbproject-68bad-default-rtdb.firebaseio.com',
-  projectId: 'dbproject-68bad',
-  storageBucket: 'dbproject-68bad.firebasestorage.app',
-  messagingSenderId: '779058037494',
-  appId: '1:779058037494:web:23b2f2bf8d67e6169ee2d7',
-  measurementId: 'G-77W6YEJ7MY',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCkxPo19SKC6V2-8LbTZ2GtxLW5CqWoePs',
+//   authDomain: 'dbproject-68bad.firebaseapp.com',
+//   databaseURL: 'https://dbproject-68bad-default-rtdb.firebaseio.com',
+//   projectId: 'dbproject-68bad',
+//   storageBucket: 'dbproject-68bad.firebasestorage.app',
+//   messagingSenderId: '779058037494',
+//   appId: '1:779058037494:web:23b2f2bf8d67e6169ee2d7',
+//   measurementId: 'G-77W6YEJ7MY',
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 //  const analytics = getAnalytics(app);
 
 export function initializeFirebase() {
